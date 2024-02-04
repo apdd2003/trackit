afc2e7b941c421e72f6e727a55e64855fe7dead5
sayeemuddin <apdd2003@yahoo.co.in>
2024-02-04 17:18:37 +0530

    update allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+7qa)*!ud@3!4ddgcymb7v6^5qr9ev9)9j#u-1b!9tswhucsuh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 # Application definition
