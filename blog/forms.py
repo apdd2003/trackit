f123c10559188e8d1e233cab9b834eed620ec959
sayeemuddin <apdd2003@yahoo.co.in>
2024-02-07 19:31:07 +0530

    update navbar, change css homepage, add placeholders in add post

--- blog/forms.py
+++ blog/forms.py
@@ -19,8 +19,8 @@ class EmailPostForm(forms.Form):
 
 
 class AddPostForm(forms.ModelForm):
-    # title = forms.CharField(max_length=255)
-    # thumbnail = forms.CharField(max_length=1024)
+    title = forms.CharField(max_length=255)
+    thumbnail = forms.CharField(max_length=1024)
     body = forms.CharField(required=False,
                            widget=CKEditorWidget())
     # tags = forms.CharField(max_length=255)
@@ -28,3 +28,11 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title',  'thumbnail', 'body', 'tags']
+
+    def __init__(self, *args, **kwargs):
+        # Call to ModelForm constructor
+        super(AddPostForm, self).__init__(*args, **kwargs)
+        self.fields['thumbnail'].widget.attrs['style'] = 'width:700px;'
+        self.fields['thumbnail'].widget.attrs['placeholder'] = 'image url'
+        self.fields['title'].widget.attrs['style'] = 'width:400px;'
+        self.fields['title'].widget.attrs['placeholder'] = 'interesting blog title'
