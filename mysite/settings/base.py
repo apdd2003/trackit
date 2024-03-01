734b5daf3cb5faeaa744f39f102e9dfefa3e1d9c
sayeemuddin <apdd2003@yahoo.co.in>
2024-03-02 00:55:15 +0530

    redis settings add

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -127,10 +127,11 @@ EMAIL_HOST_USER = 'sayeem.shaikh@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
+        'LOCATION': f'redis://{REDIS_HOST}:6379',
     }
 }
 
--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -17,7 +17,7 @@ DATABASES = {
     }
 }
 
-REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1:81')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 REDIS_URL = 'redis://cache:6379'
 CACHES['default']['LOCATION'] = REDIS_URL
